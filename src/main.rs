4cef88947a269686b8d6175a8d82e520aa0c57a2
ytyaru <ytyaru@outlook.jp>
2019-06-01 14:02:54 +0900

    初回コミット

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+/*
+ * Rustでwhile。
+ * CreatedAt: 2019-06-01
+ */
+fn main() {
+    let mut count = 0;
+    while count < 10 {
+        println!("while count {}", count);
+        count += 1;
+    }
+}
+
